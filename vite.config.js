36d8ab933c1330c990f9e6847f75f46a2f30a698
Mario <mariocraciundaniel@gmail.com>
2026-06-09 19:54:52 +0300

    fixing the base repo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'project-8',
+  base: '/project-8/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
