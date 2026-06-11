d131f1716153f5785276fbc6844282ffd48e6140
Mario <mariocraciundaniel@gmail.com>
2026-06-11 21:21:17 +0300

    fixing css for mobile

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,30 +5,17 @@ function Carousel({ images }) {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const previousImage = () => {
-        setCurrentIndex(
-            currentIndex === 0
-                ? images.length - 1
-                : currentIndex - 1
-        );
+        setCurrentIndex(currentIndex === 0 ? images.length - 1 : currentIndex - 1);
     };
 
     const nextImage = () => {
-        setCurrentIndex(
-            currentIndex === images.length - 1
-                ? 0
-                : currentIndex + 1
-        );
+        setCurrentIndex(currentIndex === images.length - 1 ? 0 : currentIndex + 1);
     };
 
     return (
         <div className="carousel">
             <button className="carousel-arrow left" onClick={previousImage}> &#10094;</button>
-
-            <img
-                src={images[currentIndex]}
-                alt=""
-                className="carousel-image"
-            />
+            <img src={images[currentIndex]} alt="" className="carousel-image" />
             <button className="carousel-arrow right" onClick={nextImage}> &#10095;</button>
 
         </div>
